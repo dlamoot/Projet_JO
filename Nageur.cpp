36cb70f707074cfaf987be1950f48e72f0651a45
dlamoot <dlamoot@outlook.fr>
2024-02-01 21:36:51 +0100

    version finale

---
+++ Nageur.cpp
@@ -0,0 +1,5 @@
+//
+// Created by David Lamoot on 01/02/2024.
+//
+
+#include "Nageur.h"
---
+++ Nageur.h
@@ -0,0 +1,8 @@
+//
+// Created by David Lamoot on 01/02/2024.
+//
+
+#ifndef SFMLTEST_NAGEUR_H
+#define SFMLTEST_NAGEUR_H
+
+#endif //SFMLTEST_NAGEUR_H
